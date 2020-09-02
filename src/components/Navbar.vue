bd23e8936c3abd94dd2f24da33368a37084bdca9
JMaylor <joseph.maylor@gmail.com>
2020-09-02 16:31:46 +0100

    no dashboard link on navbar unless logged in

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/">Dashboard</b-nav-item>
+          <b-nav-item v-if="loggedIn" to="/">Dashboard</b-nav-item>
           <b-nav-item v-if="loggedIn" to="/search">Search</b-nav-item>
         </b-navbar-nav>
 
